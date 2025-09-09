f11cc1fa0a10a3459d5f4fd139ae656def64cfa9
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-09-09 08:46:32 -0300

    09-09

---
+++ Perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'categoria',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Em uma corrida, você ultrapassa o segundo colocado. Em que posição você está agora?',
+    '2º colocado. A pegadinha é que ao ultrapassar o segundo, você fica com a posição dele, não a primeira.'
+)
+
+
+criaCartao(
+    'conhecimentos gerais',
+    'Você tem 4 filhas e cada uma delas tem 1 irmão. Quantos filhos você tem ao todo?',
+    '5 filhos. A pegadinha está em achar que são 4 filhos, quando na verdade as 4 filhas têm o mesmo único irmão.'
+)
+
+criaCartao(
+    'conhecimentos gerais',
+    'Se você tem 5 caixas, e cada caixa tem 5 bolas, e cada bola está dentro de uma pequena sacola, quantas sacolas você tem?',
+    '25 sacolas. A pegadinha aqui está em interpretar mal o número de itens: são as bolas que estão nas sacolas, não as caixas.'
+)
+criaCartao(
+    'conhecimentos gerais',
+    'Se um avião viaja a 500 km/h e precisa percorrer 500 km, quanto tempo ele levará para chegar ao destino?',
+    '1 hora. A pegadinha está na ideia de que a questão pode fazer o aluno pensar que o tempo é mais complicado do que parece.'
+)
+criaCartao(
+    'conhecimentos gerais',
+    'Qual é o elemento químico com símbolo "Na" na tabela periódica?',
+    'Sódio'
+)
+criaCartao(
+    'conhecimentos gerais',
+    'pergunta',
+    'resposta'
+)

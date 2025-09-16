8b70dffbfa80781d9df3cca5d0286e1104e90011
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-09-16 10:37:10 -0300

    16-09

--- Perguntas.js
+++ Perguntas.js
@@ -18,50 +18,60 @@ criaCartao(
     'Se você tem 5 caixas, e cada caixa tem 5 bolas, e cada bola está dentro de uma pequena sacola, quantas sacolas você tem?',
     '25 sacolas. A pegadinha aqui está em interpretar mal o número de itens: são as bolas que estão nas sacolas, não as caixas.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Se um avião viaja a 500 km/h e precisa percorrer 500 km, quanto tempo ele levará para chegar ao destino?',
     '1 hora. A pegadinha está na ideia de que a questão pode fazer o aluno pensar que o tempo é mais complicado do que parece.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Qual é o elemento químico com símbolo "Na" na tabela periódica?',
     'Sódio'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Em que ano a Primeira Guerra Mundial teve início?',
     '1914.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Em qual continente está localizada a cordilheira dos Andes?',
     'América do Sul.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Quem foi o líder da revolução russa de 1917?',
     'Vladimir Lenin.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Quais são os cinco oceanos que existem no planeta?',
     'Atlântico, Pacífico, Índico, Ártico e Antártico.'
 )
 
+
 criaCartao(
     'conhecimentos gerais',
     'Quem foi o responsável pela teoria da evolução das espécies?',
     'Charles Darwin.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'Qual país foi sede dos Jogos Olímpicos de 2020 (realizados em 2021)?',
     'Japão.'
 )
+
 criaCartao(
     'conhecimentos gerais',
     'O que são os BRICS e quais países fazem parte?',
     'Os BRICS são um grupo de cinco países emergentes: Brasil, Rússia, Índia, China e África do Sul, que colaboram em várias questões econômicas e políticas.'
 )
+
 

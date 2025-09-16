00931c6373028f222c32b2b554379cd391a713c8
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-09-16 10:42:39 -0300

    16/09

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
 
-    cartao.innertHTML =`
+    cartao.innertHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
     <div class="cartao__conteudo__pergunta">
-    <p> ${pergunta}</p>
+    <p>${pergunta}</p>
     </div>
     <div class="cartao__conteudo__resposta">
     <p>${resposta}</p>

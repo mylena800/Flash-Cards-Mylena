1a5c0f62e5c6c6565e9ed7d40820ebc7a80df87c
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-09-30 09:22:06 -0300

    330/09

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
     cartao.innertHTML = `
-    <div class="cartao__conteudo">
-    <h3>${categoria}</h3>
-    <div class="cartao__conteudo__pergunta">
-    <p>${pergunta}</p>
-    </div>
-    <div class="cartao__conteudo__resposta">
-    <p>${resposta}</p>
-    </div>
-    </div>`
-
+<div class="cartao__conteudo">
+<h3>${categoria}</h3>
+<div class="cartao__conteudo__pergunta">
+<p>${pergunta}</p>
+</div>
+<div class="cartao__conteudo__resposta">
+<p>${resposta}</p>
+</div>
+</div>`
     let respostaEstaVisivel = false
 
     function viraCartao() {

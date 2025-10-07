dab3fbd9dc5431f7f3387332c74b9bd1dbdd5b29
mylena800 <mylena.wessling.souza@escola.pr.gov.br>
2025-10-07 09:23:06 -0300

    07/10

--- Perguntas.js
+++ Perguntas.js
@@ -1,73 +1,73 @@
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Em uma corrida, você ultrapassa o segundo colocado. Em que posição você está agora?',
     '2º colocado. A pegadinha é que ao ultrapassar o segundo, você fica com a posição dele, não a primeira.'
 )
 
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Você tem 4 filhas e cada uma delas tem 1 irmão. Quantos filhos você tem ao todo?',
     '5 filhos. A pegadinha está em achar que são 4 filhos, quando na verdade as 4 filhas têm o mesmo único irmão.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Se você tem 5 caixas, e cada caixa tem 5 bolas, e cada bola está dentro de uma pequena sacola, quantas sacolas você tem?',
     '25 sacolas. A pegadinha aqui está em interpretar mal o número de itens: são as bolas que estão nas sacolas, não as caixas.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Se um avião viaja a 500 km/h e precisa percorrer 500 km, quanto tempo ele levará para chegar ao destino?',
     '1 hora. A pegadinha está na ideia de que a questão pode fazer o aluno pensar que o tempo é mais complicado do que parece.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Qual é o elemento químico com símbolo "Na" na tabela periódica?',
     'Sódio'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Em que ano a Primeira Guerra Mundial teve início?',
     '1914.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Em qual continente está localizada a cordilheira dos Andes?',
     'América do Sul.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Quem foi o líder da revolução russa de 1917?',
     'Vladimir Lenin.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Quais são os cinco oceanos que existem no planeta?',
     'Atlântico, Pacífico, Índico, Ártico e Antártico.'
 )
 
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Quem foi o responsável pela teoria da evolução das espécies?',
     'Charles Darwin.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'Qual país foi sede dos Jogos Olímpicos de 2020 (realizados em 2021)?',
     'Japão.'
 )
 
 criaCartao(
-    'conhecimentos gerais',
+    'Conhecimentos gerais',
     'O que são os BRICS e quais países fazem parte?',
     'Os BRICS são um grupo de cinco países emergentes: Brasil, Rússia, Índia, China e África do Sul, que colaboram em várias questões econômicas e políticas.'
 )
